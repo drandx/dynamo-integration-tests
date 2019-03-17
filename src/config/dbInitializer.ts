b9d7b782c2b0359b862f5610827036e17966f490
Juan Pablo Garcia <garjuanpablo@gmail.com>
2019-03-17 17:57:30 -0500

    AddCodeship file

--- src/config/dbInitializer.ts
+++ src/config/dbInitializer.ts
@@ -3,8 +3,8 @@ import * as AWS from 'aws-sdk';
 export const dynamoConnection = new AWS.DynamoDB({
     endpoint: process.env.DB_END_POINT,
     region: process.env.AWS_API_REGION,
-    accessKeyId: 'mocked',
-    secretAccessKey: 'mocked',
+    accessKeyId: process.env.AWS_API_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_API_SECRET_KEY,
 });
 
 var params = {
