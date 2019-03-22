efc44a039d88a6cb0f83c304fd50662e4a3728da
Juan Pablo Garcia <garjuanpablo@gmail.com>
2019-03-22 10:40:36 -0500

    Updated run scropt

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,5 @@
-docker stop intDynamo
-docker run --rm --name intDynamo -d -p 8000:8000 amazon/dynamodb-local
+docker-compose stop
+docker-compose up
 npm run build
 node ./dist/src/config/dbInitializer.js
 aws dynamodb list-tables --endpoint-url http://localhost:8000
